54855e231729630bcbef38d24df2c616b25625ad
wuyifan <wuyifanc@enn.com>
2019-12-17 17:13:48 +0800

    修改spring boot版本号为2.2.2

---
+++ yifan-gateway/src/test/java/com/yifan/YifanGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YifanGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ yifan-instance-c/src/main/java/com/yifa/InstanceCApplication.java
@@ -0,0 +1,13 @@
+package com.yifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstanceCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstanceCApplication.class, args);
+    }
+
+}
