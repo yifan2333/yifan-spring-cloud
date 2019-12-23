0337ea4b182a4aaf8686223c433924b58e1d4856
wuyifan <wuyifanc@enn.com>
2019-12-23 18:09:50 +0800

    hystrix 并发提高

--- yifan-gateway/src/main/java/com/yifan/endpoint/FallbackController.java
+++ yifan-gateway/src/main/java/com/yifan/endpoint/FallbackController.java
@@ -3,9 +3,6 @@ package com.yifan.endpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-
 /**
  * The type Fallback controller.
  *
@@ -23,9 +20,8 @@ public class FallbackController {
      * @date 2019年12月19日 15:59
      */
     @GetMapping("/fallback")
-    @HystrixCommand(commandProperties = {@HystrixProperty(name ="execution.isolation.thread.timeoutInMilliseconds" ,value ="3000" )})
     public String fallback() {
-        return "Hello World!\nfrom gateway";
+        return "500";
     }
 
 }
